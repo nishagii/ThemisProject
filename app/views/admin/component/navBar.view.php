dc65276a9198b26e1393194d2fe0263cde226a14
NadhiyaNashath <nashathnadhiya@gmail.com>
2024-11-21 16:07:57 +0530

    admin Nav Bar

---
+++ app/views/admin/component/navBar.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THEMIS Admin Panel</title>
+    <link rel="stylesheet" href="../../../../public/assets/css/admin/navBar.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">  <!-- this is imported to use icons -->
+   <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+
+    <body>
+        <div class="container">
+            
+            <nav>
+            
+                <img src="../../../../public/assets/images/themis_logo.png" alt="Logo" class="navbar-logo" />
+            </nav>
+
+            <div class="navbar-icons">
+                <i class="fas fa-bell"></i>
+                <a href="setting">
+                    <i class="fas fa-cog"></i>
+                </a>
+                <a href="profile">
+                    <i class="fas fa-user-circle"></i>
+                </a>
+            </div>
+            
+        </div>
+
+    </body>
+</html>
