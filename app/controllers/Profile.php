5a8e9c7d71c88aa64f908c319371a0402b6c7a85
NadhiyaNashath <nashathnadhiya@gmail.com>
2024-11-22 20:24:15 +0530

    senior Counsel user profile

---
+++ app/controllers/Profile.php
@@ -0,0 +1,16 @@
+<?php
+
+class Profile
+{
+    use Controller;
+
+    public function index()
+    {
+
+        // Set username from session, or default to 'User'
+        $data['username'] = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
+
+        // Load the view with data
+        $this->view('/seniorCounsel/profilePage', $data);
+    }
+}
