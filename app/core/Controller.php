1783c15a7038f13e780a850e09503f594e8bda51
nishagii <jeewanthadeherath04@gmail.com>
2024-11-22 10:20:50 +0530

    load model function is working fine

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -17,5 +17,17 @@ Trait Controller
 		}
 	}
 
-	
+	public function loadModel($model)
+	{
+		$modelPath = "../app/models/" . $model . ".php";
+
+		if (file_exists($modelPath)) {
+			require_once $modelPath;
+
+			// Return an instance of the model class
+			return new $model();
+		} else {
+			die("The model file {$modelPath} does not exist.");
+		}
+	}
 }
