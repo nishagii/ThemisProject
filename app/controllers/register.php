f7597e808cd7f37f886d4a960dcf9351139b25f3
NadhiyaNashath <nashathnadhiya@gmail.com>
2024-11-23 22:21:31 +0530

    commit

--- app/controllers/register.php
+++ app/controllers/register.php
@@ -6,23 +6,10 @@ class Register
 
     public function index()
     {
-        $data = [];
+
         
-        if ($_SERVER['REQUEST_METHOD'] == "POST") {
-            $user = $this->loadModel('UserModel');
-            
-            if ($user->validate($_POST)) {
-                if ($user->save($_POST)) {
-                    redirect('homelawyer');
-                    return;
-                } else {
-                    $data['errors']['general'] = "Unable to save user to the database. Please try again.";
-                }
-            } else {
-                $data['errors'] = $user->errors;
-            }
-        }
-        
-        $this->view('register', $data);
+
+        // Load the view with data
+        $this->view('/landingPage/register');
     }
 }
