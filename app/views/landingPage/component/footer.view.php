26ffdd0bd0dee3a2e24d2d6d8398c6edabfb9f7d
NadhiyaNashath <nashathnadhiya@gmail.com>
2024-11-23 13:37:51 +0530

    footer link navigation

---
+++ app/views/landingPage/component/footer.view.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THEMIS</title>
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/footer.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">  <!-- this is imported to use icons -->
+
+</head>
+<body>
+
+
+
+    <div class="footer">
+        <div class="footer-content">
+            <div class="quick-links">
+                <h4>Quick Links</h4>
+                <ul>
+                    <li><a href="landingpage">Home</a></li>
+                    <li><a href="about">About Us</a></li>
+                    <li><a href="inquire">Contact</a></li>
+                    <li><a href="practice">Practice Area</a></li>
+                    <li><a href="blog">Blog</a></li>
+                </ul>
+            </div>
+            <div class="stay-connected">
+                <img src="<?= ROOT ?>/assets/images/themis_logo.png" height="75">
+                <h4>Stay Connected with Your Lawyer</h4>
+                <p>
+                    At THEMIS, we believe that effective legal management should be accessible and user-friendly. Our platform is designed to simplify your experience, allowing you to focus on what truly matters - your case.
+                </p>
+            </div>
+            
+            
+            <div class="contact-info">
+                <h4>Contact Our THEMIS Lawyer</h4>
+                <p>076-1234567<br>
+                johnkepling@gmail.com<br>
+                63, Galle Road, Colombo-04</p>
+            </div>
+        </div>
+        <div class="social-media">
+            <h4>Follow Us</h4>
+            <div class="social-icons">
+                <a href="www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                <a href="www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="www.linkedin.com" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>Developed by VIP Software Solutions</p>
+        </div>
+    </div>
+
+</body>
+</html>
