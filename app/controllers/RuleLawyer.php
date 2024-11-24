32d2d37304ded9d475af1da6efb89e35f65dbcf2
NadhiyaNashath <nashathnadhiya@gmail.com>
2024-11-24 13:49:47 +0530

    commit

---
+++ app/controllers/RuleLawyer.php
@@ -0,0 +1,17 @@
+<?php
+
+// HomeAdmin class
+class RuleLawyer
+{
+    use Controller;
+
+    public function index()
+    {
+
+        // Set username from session, or default to 'User'
+        $data['username'] = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
+
+        // Load the view with data
+        $this->view('/seniorCounsel/rule', $data);
+    }
+}
