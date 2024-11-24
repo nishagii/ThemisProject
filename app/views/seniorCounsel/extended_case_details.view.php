562d5e623eb4fea290be8ff7f6714050746941b9
NadhiyaNashath <nashathnadhiya@gmail.com>
2024-11-24 10:50:51 +0530

    tabular view navigation

--- app/views/seniorCounsel/extended_case_details.view.php
+++ app/views/seniorCounsel/extended_case_details.view.php
@@ -5,13 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cases List</title>
-    <style>
-        /* Add your styling here */
-    </style>
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/case.css">
 </head>
 
 <body>
+<?php include('component/bigNav.view.php'); ?>
+    <?php include('component/smallNav1.view.php'); ?>
     <h1>List of Cases</h1>
+<div class="button">
+    <a href="<?= ROOT ?>/cases">
+        <button class="add">Add New Case</button>
+    </a>
+    <a href="<?= ROOT ?>/cases/retrieveAllCases">
+        <button class="add">Card View</button>
+    </a>
+</div>
     <table border="1" cellpadding="10" cellspacing="0">
         <thead>
             <tr>
