a99fc4d39d79aa499957ccf1eed7271369d51564
Chamath Abeysinghe <chamathdinisuru@gmail.com>
2024-11-25 17:58:51 +0530

    added css styling for all_precedents

--- app/views/all_precedents.view.php
+++ app/views/all_precedents.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Precedents</title>
-    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/create_precedent.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/all_precedents.css">
 </head>
 <body>
     <div class="header">
