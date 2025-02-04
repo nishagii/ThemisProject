267f3c8ada60b192f1855f048857e9f21dc714d6
nishagii <jeewanthadeherath04@gmail.com>
2025-02-04 18:41:43 +0530

    update navigation link to PaymentController and enhance payment success page design

--- app/views/client/payment_success.view.php
+++ app/views/client/payment_success.view.php
@@ -6,43 +6,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Congratulations Page Design By WebJourney - Html Template </title>
+    <title> Payment Successful</title>
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
+        /* Ensure the body takes the full height of the viewport */
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #f3f3f3;
+            /* Optional background color */
+        }
+
         .congratulation-wrapper {
             max-width: 550px;
+            width: 100%;
             margin-inline: auto;
-            -webkit-box-shadow: 0 0 20px #f3f3f3;
             box-shadow: 0 0 20px #f3f3f3;
             padding: 30px 20px;
             background-color: #fff;
             border-radius: 10px;
-        }
-
-        .congratulation-contents.center-text .congratulation-contents-icon {
-            margin-inline: auto;
+            text-align: center;
         }
 
         .congratulation-contents-icon {
-            display: -webkit-box;
-            display: -ms-flexbox;
             display: flex;
-            -webkit-box-align: center;
-            -ms-flex-align: center;
             align-items: center;
-            -webkit-box-pack: center;
-            -ms-flex-pack: center;
             justify-content: center;
             height: 100px;
             width: 100px;
-            background-color: #65c18c;
+            background-color: #ffa012;
             color: #fff;
             font-size: 50px;
             border-radius: 50%;
-            margin-bottom: 30px;
+            margin: 0 auto 30px;
         }
 
         .congratulation-contents-title {
@@ -67,22 +70,22 @@
             border: 2px solid transparent;
             border-radius: 3px;
             text-decoration: none;
-            padding: 5px;
+            padding: 10px 20px;
         }
     </style>
 </head>
 
 <body>
     <!-- Congratulations area start -->
-    <div class="congratulation-area text-center mt-5">
+    <div class="congratulation-area">
         <div class="container">
             <div class="congratulation-wrapper">
-                <div class="congratulation-contents center-text">
+                <div class="congratulation-contents">
                     <div class="congratulation-contents-icon">
                         <i class="fas fa-check"></i>
                     </div>
                     <h4 class="congratulation-contents-title">Payment Success</h4>
-                    <p class="congratulation-contents-para"> Your payment is recieved. Thank for banking with us! </p>
+                    <p class="congratulation-contents-para"> Your payment received. Thank you for paying with us ! Click botton below to go back to Home page </p>
                     <div class="btn-wrapper mt-4">
                         <a href="<?= ROOT ?>/homeclient" class="cmn-btn btn-bg-1"> Go to Home </a>
                     </div>
