96b001578dfd90f1715109683d3e667ea549da87
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-02-25 19:22:59 +0530

    get case number of clients

--- app/controllers/Invoice.php
+++ app/controllers/Invoice.php
@@ -6,12 +6,41 @@ class Invoice
     use Controller;
 
     public function index()
-
     {
-        $UserModel = $this->loadModel('UserModel'); 
-        $client = $UserModel->getAllClients(); // Fetch cases data
-        // var_dump($client); die();
+        $UserModel = $this->loadModel('UserModel');
+        $client = $UserModel->getAllClients();
         $this->view('seniorCounsel/invoice', ['client' => $client]);
+    }
 
+    public function getCaseNumberByClient()
+{
+    $data = json_decode(file_get_contents('php://input'), true);
+    
+    // Log to see if the email is correctly passed
+    error_log("Received data: " . print_r($data, true));  // This will log the input data
+
+    if (isset($data['email'])) {
+        $clientEmail = $data['email'];
+        $CaseModel = $this->loadModel('CaseModel');
+
+        // Fetch case numbers for the client based on email
+        $caseNumbers = $CaseModel->getCaseNumbersByEmail($clientEmail);
+
+        if ($caseNumbers) {
+            // Return JSON response with case numbers
+            echo json_encode(['caseNumbers' => $caseNumbers]);
+        } else {
+            // Return an empty array if no case numbers were found
+            echo json_encode(['caseNumbers' => []]);
+        }
+    } else {
+        // Handle the case when no email is provided
+        echo json_encode(['error' => 'No email provided']);
     }
 }
+
+
+    
+}
+
+
