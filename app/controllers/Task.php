dd50b327ccd9ec972e03af04a013b2d6e7c97792
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-04-06 16:03:30 +0530

    junior counsel task page protected routes

--- app/controllers/Task.php
+++ app/controllers/Task.php
@@ -1,17 +1,19 @@
 <?php
 
-
 class Task
 {
     use Controller;
 
     public function index()
     {
+        if (empty($_SESSION['user_id'])) {
+            redirect('login');
+            return;
+        }
 
-       
-        $data['username'] = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
+        $data['username'] = $_SESSION['username'] ?? 'User';
 
-        
+        // Load the view
         $this->view('/juniorCounsel/task', $data);
     }
 }
