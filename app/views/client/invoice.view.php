1648ded683de982242de69bb9ef0ce1d902fe03e
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-04-11 14:55:19 +0530

    invoice view for client

---
+++ app/views/client/invoice.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoice</title>
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/client/payments.css">
+    <script src="https://js.stripe.com/v3/"></script> <!-- Stripe.js -->
+</head>
+
+<body>
+    <?php include('component/bigNav.view.php'); ?>
+    <?php include('component/smallNav1.view.php'); ?>
+    <?php include(__DIR__ . '/../seniorCounsel/component/sidebar.view.php'); ?>
+
+    
+
+    <div class="home-section">
+       
+       
+        
+    </div>
+
+        
+</body>
+
+</html>
