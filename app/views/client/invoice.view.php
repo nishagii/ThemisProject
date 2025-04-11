5ee7f72417c4d1e218fbb80878c3b744b8d136cc
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-04-11 15:27:35 +0530

    invoice view page for client is done

--- app/views/client/invoice.view.php
+++ app/views/client/invoice.view.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Invoice</title>
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/client/payments.css">
-    <script src="https://js.stripe.com/v3/"></script> <!-- Stripe.js -->
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/client/invoice.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 </head>
 
 <body>
@@ -18,8 +20,48 @@
 
     <div class="home-section">
        
-       
-        
+        <div class="invoice-table">
+            <div class="table-div">
+                <div class="transaction-header">
+                    <div>Payment Amount</div>
+                    <div>Description</div>
+                    <div>Sent Date</div>
+                    <div>Due Date</div>
+                    <div>Download</div>
+                    <div>Proceed to Pay</div>
+                </div>
+
+                <!-- Updated transactions -->
+                <div class="transaction-row">
+                    <div class="amount">$250</div>
+                    <div class="transaction-description">Invoice for Legal Consultation</div>
+                    <div class="transaction-sent-date">2025-04-10</div>
+                    <div class="due-date">2025-04-17</div>
+                    <div><a href="<?= ROOT ?>/assets/documents/invoice1.pdf" download class="download-button">Download</a></div>
+                    <button class="pay-button"><i class='bx bx-right-arrow-circle'></i></button>
+
+                </div>
+
+                <div class="transaction-row">
+                    <div class="amount">$75</div>
+                    <div class="transaction-description">Court Filing Receipt</div>
+                    <div class="transaction-sent-date">2025-04-05</div>
+                    <div class="due-date">2025-04-12</div>
+                    <div><a href="<?= ROOT ?>/assets/documents/filing_receipt.pdf" download class="download-button">Download</a></div>
+                    <button class="pay-button"><i class='bx bx-right-arrow-circle'></i></button>
+                </div>
+
+                <div class="transaction-row">
+                    <div class="amount">$120</div>
+                    <div class="transaction-description">Evidence Document</div>
+                    <div class="transaction-sent-date">2025-03-30</div>
+                    <div class="due-date">2025-04-06</div>
+                    <div><a href="<?= ROOT ?>/assets/documents/evidence_doc.pdf" download class="download-button">Download</a></div>
+                    <button class="pay-button"><i class='bx bx-right-arrow-circle'></i></button>
+                </div>
+            </div>
+
+        </div>
     </div>
 
         
