fdf0af15d412d598385ddc44402e59a0f20b26c1
sawani-git <sawanivihanga@gmail.com>
2025-04-20 16:05:30 +0530

    blog model is created

---
+++ app/models/BlogMode.php
@@ -0,0 +1,31 @@
+<?php
+
+class BlogModel {
+    use Model;
+
+    protected $table = 'blogs';
+
+    public function save($data)
+    {
+        try {
+            $query = "INSERT INTO {$this->table}
+                      (title, content, image_url, author)
+                      VALUES
+                      (:title, :content, :image_url, :author)";
+
+            $params = [
+                'title' => $data['title'],
+                'content' => $data['content'],
+                'image_url' => $data['image_url'],
+                'author' => $data['author'],
+            ];
+
+            return $this->query($query, $params);
+        } catch (Exception $e) {
+            error_log("Database Error [BlogModel:save]: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    
+}
