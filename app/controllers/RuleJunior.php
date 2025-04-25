d284857096426dcb3acb54ebb0384b7c2f002520
Chamath Abeysinghe <chamathdinisuru@gmail.com>
2025-04-25 23:34:01 +0530

    sc rules bug fixed

--- app/controllers/RuleJunior.php
+++ app/controllers/RuleJunior.php
@@ -7,11 +7,13 @@ class RuleJunior
 
     public function index()
     {
-
         // Set username from session, or default to 'User'
         $data['username'] = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
 
+        $rulesModel = $this->loadModel('SCrulesModel');
+        $rules = $rulesModel->getAll();
+
         // Load the view with data
-        $this->view('/juniorcounsel/rule', $data);
+        $this->view('/juniorcounsel/rule',['rules' => $rules]);
     }
 }
