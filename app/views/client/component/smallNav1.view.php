0f89ba7666a08f867400cda79a7c6d71b172048f
Nishagi <jeewanthadeherath04@gmail.com>
2025-04-26 20:34:14 +0530

    fix: update navigation links to use ROOT constant for correct URL paths

--- app/views/client/component/smallNav1.view.php
+++ app/views/client/component/smallNav1.view.php
@@ -14,9 +14,9 @@
 <body>
     <div>
         <ul class="small-navbar">
-            <li><a href="CalendarClient">Calendar</a></li>
-            <li><a href="templateclient">Document templates</a></li>
-            <li><a href="meetingClient">Meeting</a></li>
+            <li><a href="<?= ROOT ?>/Calendar">Calendar</a></li>
+            <li><a href="<?= ROOT ?>/templateclient">Document templates</a></li>
+            <li><a href="<?= ROOT ?>/meetingClient">Meeting</a></li>
             <li><a href="<?= ROOT ?>/SCrules/retrieveForView">S.C Rules</a></li>
         </ul>
     </div>
